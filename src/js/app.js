7221068c1e1068e79001b2e1e1a8051cc3237df7
nomech <mech1803@gmail.com>
2025-02-09 23:31:15 +0100

    Add form section, add logic to display section conditionally

--- src/js/app.js
+++ src/js/app.js
@@ -10,9 +10,33 @@ const deleteCancelButton = document.querySelector(
   ".delete-modal__button--cancel"
 );
 
+const typeFiled = document.querySelector(".form__input--type");
+
+const prescriptionSection = document.querySelector(
+  ".form__group--prescription"
+);
+const prescriptionFields = [
+  document.querySelector(".form__input--dosage"),
+  document.querySelector(".form__input--frequency"),
+];
+
+const otcSection = document.querySelector(".form__group--otc");
+const otcFields = [
+  document.querySelector(".form__input--price"),
+  document.querySelector(".form__input--age"),
+];
 
 document.addEventListener("DOMContentLoaded", () => {
   Ui.openModal(addButton, formModal, cancelButton);
   Ui.openModal(deleteButton, deleteModal, deleteCancelButton);
 
+  typeFiled.addEventListener("change", () => {
+    Ui.toggleMedicineSection(
+      prescriptionSection,
+      prescriptionFields,
+      otcSection,
+      otcFields,
+      typeFiled
+    );
+  });
 });
--- src/js/ui.js
+++ src/js/ui.js
@@ -12,6 +12,32 @@ class Ui {
       modal.style.display = "none";
     });
   }
+
+  static toggleMedicineSection(
+    prescriptionSection,
+    prescriptionFields,
+    otcSection,
+    otcFields,
+    select
+  ) {
+    prescriptionSection.style.display = "none";
+    otcSection.style.display = "none";
+    prescriptionFields.forEach((field) => {
+      field.value = "";
+    });
+    otcFields.forEach((field) => {
+      field.value = "";
+    });
+
+    if (select.value === "prescription") {
+      prescriptionSection.style.display = "inherit";
+    } else if (select.value === "otc") {
+      otcSection.style.display = "inherit";
+    } else {
+      prescriptionSection.style.display = "none";
+      otcSection.style.display = "none";
+    }
+  }
 }
 
 export default Ui;
