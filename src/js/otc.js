2a0000b14bd9c9567918169befc837a4bea5998b
nomech <mech1803@gmail.com>
2025-02-10 10:47:09 +0100

    Add base class and extended classes

---
+++ src/js/otc.js
@@ -0,0 +1,9 @@
+import Pharmaceuticals from "./pharmaceuticals.js";
+
+class OTC extends Pharmaceuticals {
+  constructor(name, manufacturer, expiryDate, quantity, age, price) {
+    super(name, manufacturer, expiryDate, quantity);
+    this.age = age;
+    this.price = price;
+  }
+}
---
+++ src/js/pharmaceuticals.js
@@ -0,0 +1,11 @@
+class Pharmaceuticals {
+  constructor(name, manufacturer, expiryDate, quantity) {
+    this.name = name;
+    this.manufacturer = manufacturer;
+    this.expiryDate = expiryDate;
+    this.quantity = quantity;
+  }
+}
+
+
+export default Pharmaceuticals;
