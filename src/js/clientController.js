6eb073816028de1a0a1b0caa4e18075d560435b3
Christoffer <mech1803@gmail.com>
2025-02-11 22:58:48 +0100

    Add logic to store data in localStorage

--- src/js/clientController.js
+++ src/js/clientController.js
@@ -2,25 +2,80 @@ import OTC from "./otc.js";
 import Prescription from "./prescription.js";
 
 class ClientController {
-  static addProduct(name,manufacturer,expiryDate,quantity,type,age,price,dosage,frequency) {
-    this.product = null;
+  static products = JSON.parse(localStorage.getItem("products")) || {
+    all: [],
+    otc: [],
+    prescription: [],
+  };
+
+  static addProduct(
+    name,
+    manufacturer,
+    expiryDate,
+    quantity,
+    type,
+    age,
+    price,
+    dosage,
+    frequency
+  ) {
     if (type === "otc") {
-      this.product = this.addOTC(name,manufacturer,expiryDate,quantity,type,age,price);
+      this.product = this.addOTC(
+        name,
+        manufacturer,
+        expiryDate,
+        quantity,
+        type,
+        age,
+        price
+      );
+      ClientController.products.otc.push(this.product);
+      ClientController.products.all.push(this.product);
     } else if (type === "prescription") {
-      this.product = this.addPrescription(name,manufacturer,expiryDate,quantity,type,dosage,frequency);
+      this.product = this.addPrescription(
+        name,
+        manufacturer,
+        expiryDate,
+        quantity,
+        type,
+        dosage,
+        frequency
+      );
+      ClientController.products.prescription.push(this.product);
+      ClientController.products.all.push(this.product);
     } else {
       console.log("Invalid type");
     }
+
+    this.saveProducts(ClientController.products);
   }
 
   static addOTC(name, manufacturer, expiryDate, quantity, age, price) {
-    const otc = new OTC(name, manufacturer, expiryDate, quantity, age, price);
-    console.log(otc);
+    return new OTC(name, manufacturer, expiryDate, quantity, age, price);
   }
 
-  static addPrescription(name,manufacturer,expiryDate,quantity,type,dosage,frequency) {
-    const prescription = new Prescription(name,manufacturer,expiryDate,quantity,type,dosage,frequency);
-    console.log(prescription);
+  static addPrescription(
+    name,
+    manufacturer,
+    expiryDate,
+    quantity,
+    type,
+    dosage,
+    frequency
+  ) {
+    return new Prescription(
+      name,
+      manufacturer,
+      expiryDate,
+      quantity,
+      type,
+      dosage,
+      frequency
+    );
+  }
+
+  static saveProducts() {
+    localStorage.setItem("products", JSON.stringify(this.products));
   }
 }
 
