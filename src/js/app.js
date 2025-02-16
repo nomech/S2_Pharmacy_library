6c93b437d185a528af974fd9664cb1c00db7718a
nomech <mech1803@gmail.com>
2025-02-17 00:08:24 +0100

    Fix render issues when adding or deleting item in specific tab

--- src/js/app.js
+++ src/js/app.js
@@ -151,7 +151,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
       }
       Ui.closeOnSubmit(formModal, prescriptionSection, otcSection);
-      Ui.renderData();
+      Ui.renderData(Ui.currentTab);
     }
   });
 });
